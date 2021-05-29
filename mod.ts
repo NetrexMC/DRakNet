78007eda2b8b69ce8f9b774f357e87aac023f868
Olybear9 <olybear9@gmail.com>
2021-05-28 23:45:57 -0500

    ✏ script: Test script

---
+++ mod.ts
@@ -0,0 +1,11 @@
+// # // This is typesharp code
+// # import typesharp;
+// # import typesharp.engines.v8_transpiler as v8;
+// # import std.fs;
+// #
+// # const engine = new v8.utils.Isolate();
+// # const code: v8.Scope = typesharp.cli_core.readCombineSync("./src", {
+// #     context: super,
+// #     excludes: ["current_context"]
+// # });
+// # const byteCode = v8.script.compile(code)
